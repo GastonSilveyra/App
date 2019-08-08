e4f5014a4ce2572ea50fe0370f8f214e1dc02980
Matias Corvalan <matias_corvalan@outlook.com>
2019-08-08 00:01:32 -0300

    register page updated

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import {AuthService } from '../services/auth.service';
 import { User } from '../shared/user.class';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { ToastController, AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
@@ -9,19 +11,34 @@ import { User } from '../shared/user.class';
 })
 export class RegisterPage implements OnInit {
   user: User = new User();
-  constructor(private authScv: AuthService, private router: Router) { 
+  // tslint:disable-next-line:max-line-length
+  constructor( private router: Router, private fireauth: AngularFireAuth, private toastController: ToastController, private alertContrller: AlertController) {
 
    }
 
   ngOnInit() {
   }
 
-async onRegister(){
-  const user = await this.authScv.onRegister(this.user);
-  if(user){
-    console.log('Successfully created user!');
-    this.router.navigateByUrl('/');
+async onRegister() {
+  const user = await this.fireauth.auth.signInWithEmailAndPassword(this.user.email, this.user.password)
+    .then(res => {
+      if (res.user) {
+        console.log(res.user);
+        this.router.navigateByUrl('/home');
+      }
+    })
+    .catch(err => {
+      console.log(`login failed ${err}`);
+      this.presentToast(err);
+    });
   }
-}
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 2000
+    });
+    toast.present();
+  }
+
 
 }

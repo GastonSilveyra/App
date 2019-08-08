d449b54dc5558fb87dfd1cbf6265dd742a946819
Matias Corvalan <matias_corvalan@outlook.com>
2019-08-08 00:01:11 -0300

    home page updated

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import {User} from '../shared/user.class';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -10,17 +12,36 @@ import {User} from '../shared/user.class';
 })
 export class HomePage implements OnInit {
 
-user : User =new User();
-  constructor(private router: Router, private authSvc: AuthService) {}
+  user: User = new User();
+  error = '';
+  image = 'assets/logo.jpg';
+  // tslint:disable-next-line:max-line-length
+  constructor(private router: Router, private authSvc: AuthService, private fireauth: AngularFireAuth, private toastController: ToastController, private alertContrller: AlertController) {}
 
   ngOnInit() {
   }
 
- async onLogin(){
-    const user= await this.authSvc.onLogin(this.user);
-    if(user){
-      console.log('Successfully logged in!');
-      this.router.navigateByUrl('/foto-pie');
+ async onLogin() {
+   this.fireauth.auth.signInWithEmailAndPassword(this.user.email, this.user.password)
+   .then(res => {
+    if (res.user) {
+      console.log(res);
+      this.presentToast('Successfully logged in!');
+      this.router.navigate(['/foto-pie']);
     }
+  })
+  .catch(err => {
+    console.log(`login failed ${err}`);
+    this.presentToast(err);
+  });
+  }
+
+  async presentToast(me: string) {
+    const toast = await this.toastController.create({
+      color: 'dark',
+      duration: 2000,
+      message: me,
+    });
+    toast.present();
   }
 }

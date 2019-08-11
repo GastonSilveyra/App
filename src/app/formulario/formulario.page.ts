2a86a0d50a6f659a5863209a7f7ada9445f1c230
mariela <gaston.silveyra87@gmail.com>
2019-08-11 19:57:42 -0300

    create PDF and email added

--- src/app/formulario/formulario.page.ts
+++ src/app/formulario/formulario.page.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { EmailComposer } from '@ionic-native/email-composer/ngx';
 import { Fotos } from '../fotos';
 
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.page.html',
@@ -11,6 +15,13 @@ export class FormularioPage implements OnInit {
 
   imagenPie: string;
   imagenTobillo: string;
+
+  letterObj = {
+    to: '',
+    from: '',
+    text: ''
+  };
+  pdfObj = null;
 
   constructor( private fotos: Fotos, private emailComposer: EmailComposer ) { }
 
@@ -31,4 +42,52 @@ export class FormularioPage implements OnInit {
     };
     this.emailComposer.open(email);
   }
+
+  crearPDF() {
+    const docDefinition = {
+      content: [
+        { text: 'REMINDER', style: 'header' },
+        { text: new Date().toTimeString(), alignment: 'right' },
+        { text: new Date().toLocaleTimeString() },
+
+        { text: 'From', style: 'subheader' },
+        { text: this.letterObj.from },
+
+        { text: 'To', style: 'subheader' },
+        { image: this.imagenPie },
+        { image: this.imagenTobillo },
+
+        { text: this.letterObj.text, style: 'story', margin: [0, 20, 0, 20] },
+
+        {
+          ul: [
+            'Bacon',
+            'Rips',
+            'BBQ',
+          ]
+        }
+      ],
+      styles: {
+        header: {
+          fontSize: 18,
+          bold: true,
+        },
+        subheader: {
+          fontSize: 14,
+          bold: true,
+          margin: [0, 15, 0, 0]
+        },
+        story: {
+          italic: true,
+          alignment: 'center',
+          width: '50%',
+        }
+      }
+    };
+    const pdfGenerator = pdfMake.createPdf(docDefinition);
+    pdfGenerator.getBase64((data) => {
+    this.pdfObj = data;
+  });
+  }
+
 }
